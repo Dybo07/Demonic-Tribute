26850901ba430e95c3c34412a26c3d9fb6878041
Dybo07 <dybo07.epic@gmail.com>
2024-02-15 13:19:40 +0100

    added Project folder and script
    added movement script
    added camera script
    added Tamas inventory script (unfinished)
    added new folder
    added Jump script (unfinished)

---
+++ Demonic Tribute/Assets/Scripts/Player movement/Player Jump.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public int jumpForce;
+    public float jumpHold;
+    public float jumpSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            
+
+        }
+        
+    }
+}
---
+++ Demonic Tribute/Assets/Scripts/Player movement/Simple Player Movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerMovement : MonoBehaviour
+{
+    public float vert;
+    public float hori;
+    public Vector3 dir;
+    public float speed;
+    //test
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hori = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+        dir.x = hori;
+        dir.z = vert;
+        transform.Translate(dir * speed * Time.deltaTime);
+
+        if (Input.GetButton("Sprint"))
+        {
+            speed = 10;
+        }
+        else
+        {
+            speed = 5;
+        }
+    }
+}
