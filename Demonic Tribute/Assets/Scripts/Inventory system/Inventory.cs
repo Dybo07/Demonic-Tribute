e6df55bc771329a4f10cb83a5a334cee371de3f9
TamasHertogs <118199296+TamasssH@users.noreply.github.com>
2024-03-07 10:54:06 +0100

    test sprites + code changes.
    Een test sprite toegevoegt en wat niewe code.

--- Demonic Tribute/Assets/Scripts/Inventory system/Inventory.cs
+++ Demonic Tribute/Assets/Scripts/Inventory system/Inventory.cs
@@ -1,14 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public List<Item> items = new List<Item>();
+    public List<Item> items = new List<Item>(18);
+
     public Item item;
+    public InventoryButton button;
+
+    public int itemCount;
+    public int maxItemCount;
+
+    public void Start()
+    {
+        maxItemCount = 17;
+    }
 
     public void AddItem(Item item)
     {
-        items.Add(item);
+        if (itemCount <= maxItemCount) 
+        {
+            itemCount++;
+            items.Insert(button.btnIndex, item);
+        } else if(itemCount >= maxItemCount)
+        {
+            Debug.Log("Max item count reached.");
+        }
     }
+
+    public void DelItem(Item item) 
+    {
+        if (itemCount <= 0)
+        {
+            Debug.Log("Cant delete item. list contains 0 items.");
+        }
+        else 
+        {
+  
+            items.RemoveAt(button.btnIndex);
+            Debug.Log("Item: " + item + "removed.");
+        }
+    }
+
 }
+
--- Demonic Tribute/Assets/Scripts/Inventory system/InventoryButton.cs
+++ Demonic Tribute/Assets/Scripts/Inventory system/InventoryButton.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -9,14 +10,42 @@ public class InventoryButton : MonoBehaviour, IPointerClickHandler
 {
     public Inventory inventory;
     public Item item;
+    public int btnIndex;
+
+    //public Sprite itemSprite;
+    //public Sprite slotImage;
+    //public Sprite defaultSprite;
+
+    public bool isClicked = false;
     public void Start()
     {
+        //defaultSprite = slotImage;
+        //itemSprite = item.icon;
+    }
 
+    public int buttonIndex() 
+    {
+        return btnIndex;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("Bababooey");
-        inventory.AddItem(item);
+        
+        if (isClicked == false)
+        {
+            inventory.AddItem(item);
+            //idfk
+            //slotImage = itemSprite;
+            //gameObject.GetComponent<RawImage>().texture = slotImage;
+            isClicked = !isClicked;
+        }
+        else if (isClicked == true) 
+        {
+            inventory.DelItem(item);
+            //idfk
+            //slotImage = defaultSprite;
+            isClicked = !isClicked;
+        }
+        
     }
 }
