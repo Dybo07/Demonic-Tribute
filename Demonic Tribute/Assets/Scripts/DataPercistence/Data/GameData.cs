e7c5e72fd95df182480f9322728034b01707fa27
Despair0090 <benniemeinen4@gmail.com>
2024-03-13 13:56:16 +0100

    save and load update
    updated the save and load system and i updated the ui.

---
+++ Demonic Tribute/Assets/Scripts/DataPercistence/Data/GameData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData 
+{
+    public int daycount;
+    
+    public GameData()
+    {
+        this.daycount = 0;
+    }
+
+    
+
+}
