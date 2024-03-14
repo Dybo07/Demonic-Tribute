bb6f5364cde46254d1db88da4b1989c706116de1
Dybo07 <dybo07.epic@gmail.com>
2024-03-14 15:22:51 +0100

    Added terrain textures and  script changes
    Added terrain texture
    Change the itemspawn script (Works now and its random)
    change the first person movement to work and it can jump and dash in first peron

--- Demonic Tribute/Assets/Scripts/Item Spawn script.cs
+++ Demonic Tribute/Assets/Scripts/Item Spawn script.cs
@@ -13,19 +13,41 @@ public class ItemSpawnscript : MonoBehaviour
     public GameObject item4;
     public GameObject item5;
 
+    public Vector3 spawnPoint;
     public int randomNum;
-    
+    public Quaternion randomQuaternion;
+    public float randomY;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPoint = gameObject.transform.position;
+        randomY = Random.Range(1,361);
+        randomQuaternion = Quaternion.Euler(0,randomY,0);
+
+        randomNum = Random.Range(1, 6);
+
+        switch(randomNum)
+        {
+            case 5:
+                Instantiate(item5, spawnPoint, randomQuaternion);
+                break;
+            case 4:
+                Instantiate(item4, spawnPoint, randomQuaternion);
+                break;
+            case 3:
+                Instantiate(item3, spawnPoint, randomQuaternion);
+                break;
+            case 2:
+                Instantiate(item2, spawnPoint, randomQuaternion);
+                break;
+            case 1:
+                Instantiate(item1, spawnPoint, randomQuaternion);
+                break;
+            default:
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
 
 }
