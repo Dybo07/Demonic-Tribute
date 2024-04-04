fa09552abbbbbea5f7a37bd6b7e9ebd1ef7f4c9c
Despair0090 <benniemeinen4@gmail.com>
2024-04-04 10:17:26 +0200

    changed a few things in script
    minor changes

--- Demonic Tribute/Assets/Scripts/DataPercistence/Data/PlayerManager.cs
+++ Demonic Tribute/Assets/Scripts/DataPercistence/Data/PlayerManager.cs
@@ -6,43 +6,34 @@ public class PlayerManager : MonoBehaviour
     
     public static PlayerManager instance;
     public int dayCounter;
-    public float scoreCounter;
+    public int scoreCounter;
     public Vector3 pos;
     public Transform player;
     public void Start()
     {
-        if(instance == null)
-        {
-            instance = this;
-        }
+        instance = this;
         player = GameObject.FindGameObjectWithTag("Player").transform;
-    }
-
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            LoadData();
-        }
-    }
-    public void LoadData()
-    {
-        dayCounter = PlayerPrefs.GetInt("days");
-        scoreCounter = PlayerPrefs.GetFloat("score");
-        player.position = new Vector3((PlayerPrefs.GetFloat("posX")), (PlayerPrefs.GetFloat("posY")), (PlayerPrefs.GetFloat("posZ")));
-
     }
 
     public void SaveData()
     {
         PlayerPrefs.SetInt("days", dayCounter);
-        PlayerPrefs.SetFloat("score", scoreCounter);
+        PlayerPrefs.SetInt("score", scoreCounter);
 
         pos = player.position;
         PlayerPrefs.SetFloat("posX", pos.x);
         PlayerPrefs.SetFloat("posY", pos.y);
         PlayerPrefs.SetFloat("posZ", pos.z);
     }
+    public void LoadData()
+    {
+        DayCounter.instance.day = PlayerPrefs.GetInt("days");
+        scoreCounter = PlayerPrefs.GetInt("score");
+        player.position = new Vector3((PlayerPrefs.GetFloat("posX")), (PlayerPrefs.GetFloat("posY")), (PlayerPrefs.GetFloat("posZ")));
+
+    }
+
+    
 
     public void DeleteData()
     {
