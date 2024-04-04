f895f7a418d04a9707532d22a782b544216eb42b
Despair0090 <benniemeinen4@gmail.com>
2024-04-04 10:52:37 +0200

    Revert "changed a few things in script"
    This reverts commit fa09552abbbbbea5f7a37bd6b7e9ebd1ef7f4c9c.

--- Demonic Tribute/Assets/Scripts/DataPercistence/Data/PlayerManager.cs
+++ Demonic Tribute/Assets/Scripts/DataPercistence/Data/PlayerManager.cs
@@ -6,34 +6,43 @@ public class PlayerManager : MonoBehaviour
     
     public static PlayerManager instance;
     public int dayCounter;
-    public int scoreCounter;
+    public float scoreCounter;
     public Vector3 pos;
     public Transform player;
     public void Start()
     {
-        instance = this;
+        if(instance == null)
+        {
+            instance = this;
+        }
         player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            LoadData();
+        }
+    }
+    public void LoadData()
+    {
+        dayCounter = PlayerPrefs.GetInt("days");
+        scoreCounter = PlayerPrefs.GetFloat("score");
+        player.position = new Vector3((PlayerPrefs.GetFloat("posX")), (PlayerPrefs.GetFloat("posY")), (PlayerPrefs.GetFloat("posZ")));
+
     }
 
     public void SaveData()
     {
         PlayerPrefs.SetInt("days", dayCounter);
-        PlayerPrefs.SetInt("score", scoreCounter);
+        PlayerPrefs.SetFloat("score", scoreCounter);
 
         pos = player.position;
         PlayerPrefs.SetFloat("posX", pos.x);
         PlayerPrefs.SetFloat("posY", pos.y);
         PlayerPrefs.SetFloat("posZ", pos.z);
     }
-    public void LoadData()
-    {
-        DayCounter.instance.day = PlayerPrefs.GetInt("days");
-        scoreCounter = PlayerPrefs.GetInt("score");
-        player.position = new Vector3((PlayerPrefs.GetFloat("posX")), (PlayerPrefs.GetFloat("posY")), (PlayerPrefs.GetFloat("posZ")));
-
-    }
-
-    
 
     public void DeleteData()
     {
