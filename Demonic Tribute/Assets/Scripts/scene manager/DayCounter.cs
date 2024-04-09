3d03cce57e74a4c4a01fc39afb9482b820a63a8f
TamasHertogs <118199296+TamasssH@users.noreply.github.com>
2024-04-09 13:34:16 +0200

    House textures.
    de house textures.

--- Demonic Tribute/Assets/Scripts/scene manager/DayCounter.cs
+++ Demonic Tribute/Assets/Scripts/scene manager/DayCounter.cs
@@ -11,8 +11,6 @@ public class DayCounter : MonoBehaviour
 {
     public static DayCounter instance;
     [Header("Days variables")]
-    [SerializeField]
-    public int day;
     public TMP_Text daycount;
 
     //public ScoreManager scoreManager;   
@@ -22,7 +20,11 @@ public class DayCounter : MonoBehaviour
     public int timeMin;
     public int timeSec;
     public bool timerOn = false;
+    public int day;
+
     public TMP_Text timeCounter;
+
+    public GameObject thePlayer;
 
 
     [Header("Score variables")]
@@ -53,18 +55,22 @@ public class DayCounter : MonoBehaviour
             day++;
             Debug.Log(day);
             ScoreManager.instance.offerItem.offerCount = 0;
+            Debug.Log(day);
             //deze singleton werkt niet.
             //PlayerManager.instance.dayCounter = day;
+            Debug.Log(day);
             StartCoroutine (DayCount());
+            Debug.Log(day);
         }
     }
 
     
     IEnumerator UpdateTimer()
     {
-       yield return new WaitForSeconds(1);
-       timeLeft -= 1;
-        if (timeLeft >= 0)
+        yield return new WaitForSeconds(1);
+        timeLeft -= 1;
+
+        if (timeLeft > 0)
         {
             StartCoroutine(UpdateTimer());
         }
@@ -89,9 +95,10 @@ public class DayCounter : MonoBehaviour
 
     IEnumerator DayCount()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         ScoreManager.instance.offerAmount += 10;
-        Debug.Log(ScoreManager.instance.offerAmount);
+        thePlayer.transform.position = new Vector3(0f, 101.08f, 0f);
+        //Debug.Log(ScoreManager.instance.offerAmount);
 
         yield return new WaitForSeconds(1);
     }
